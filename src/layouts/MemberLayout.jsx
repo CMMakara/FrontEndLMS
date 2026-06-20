9ad9009d229c69541d6703f66ef31fbf78898b86
CMMakara <makaragamer91@gmai.com>
2026-06-20 22:18:11 +0700

    fix

--- src/layouts/MemberLayout.jsx
+++ src/layouts/MemberLayout.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Navbar from "../components/layout/Member/Navbar";
 import Footer from "../components/layout/Member/footer";
-function MemberLayout({ children }) {
+import { Outlet } from "react-router-dom";
+function MemberLayout() {
   return (
     <div className="min-vh-100 d-flex flex-column bg-light">
       {/* ១. របារ Navigation ខាងលើ */}
       <Navbar />
 
       {/* ២. ផ្ទៃមាតិកា (កន្លែងប្តូរទៅតាមទំព័រនីមួយៗ) */}
-      <main className="flex-grow-1 py-4">
-        {children} {/* មាតិកាពី Homepage នឹងត្រូវមកបង្ហាញនៅចំចំណុចនេះ */}
+      <main>
+        <Outlet/>
       </main>
 
       {/* ៣. របារ Footer ខាងក្រោម */}
