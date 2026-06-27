0548d960a78a2730b173291220293f92f72820ca
CMMakara <makaragamer91@gmai.com>
2026-06-27 11:28:33 +0700

    fix all

--- src/services/userService.js
+++ src/services/userService.js
@@ -41,3 +41,38 @@ export const getMeAPI = async () =>{
     throw error;
   }
 }
+
+export const updateProfileAPI = async (data) =>{
+  try {
+    let res = await api.put('/profile/info', data)
+    return res.data
+  } catch (error) {
+    console.log(error)
+    throw error;
+  }
+}
+
+export const updateProfileImageAPI =  async(file) =>{
+  try {
+    const formData = new FormData();
+    formData.append("profile_image", file);
+    let res = await api.post('/profile/image' , formData , {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    return res.data
+  } catch (error) {
+    console.log(error)
+    throw error;
+  }
+}
+
+export const deleteProfileAPI = async () =>{
+  try {
+    let res =await api.delete('/profile/image')
+    return res.data
+  } catch (error) {
+    
+  }
+}
